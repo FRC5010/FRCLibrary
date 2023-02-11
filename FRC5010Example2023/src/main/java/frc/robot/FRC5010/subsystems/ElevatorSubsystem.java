0e86ba832eebe0dc2e0c2f19585e2b422eb946e3
MasonHarmon <113945967+MasonHarmon@users.noreply.github.com>
2023-02-11 12:35:04 -0500

    Added Mechanism2d

--- FRC5010Example2023/src/main/java/frc/robot/FRC5010/subsystems/ElevatorSubsystem.java
+++ FRC5010Example2023/src/main/java/frc/robot/FRC5010/subsystems/ElevatorSubsystem.java
@@ -9,10 +9,17 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.FRC5010.constants.ElevatorConstants;
 import frc.robot.FRC5010.constants.GenericPID;
-import frc.robot.FRC5010.motors.hardware.NEO; 
+import frc.robot.FRC5010.motors.hardware.NEO;
+import frc.robot.FRC5010.sensors.encoder.GenericEncoder;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorSubsystem extends SubsystemBase {
   private NEO lift;
@@ -21,14 +28,24 @@ public class ElevatorSubsystem extends SubsystemBase {
   private ElevatorConstants elevatorConstants;
   private double KFF = 0.000156;
   private double kIz = 0;
+  private Mechanism2d m_mech2d;
+  private MechanismRoot2d m_mech2dRoot;
+  private MechanismLigament2d m_elevatorMech2d;
+  private GenericEncoder encoder;
   //TODO Implement ElevatorFeefForward
   private ElevatorFeedforward feedforward;
-  public ElevatorSubsystem(NEO lift, GenericPID liftconstants, ElevatorConstants elevatorConstants) {
+  public ElevatorSubsystem(NEO lift, GenericPID liftconstants, ElevatorConstants elevatorConstants, Mechanism2d mech2d) {
   this.lift = lift;
   this.liftController = lift.getPIDController(); 
+  this.encoder = lift.getMotorEncoder();
   this.liftconstants = liftconstants;
   this.elevatorConstants = elevatorConstants;
-
+  this.m_mech2d = mech2d;
+  m_mech2dRoot = m_mech2d.getRoot("Elevator Root", 10, 0);
+  m_elevatorMech2d =
+      m_mech2dRoot.append(
+          new MechanismLigament2d(
+              "Elevator", Units.metersToInches(0), 90));
   feedforward = new ElevatorFeedforward(elevatorConstants.getkS(), elevatorConstants.getkV(), elevatorConstants.getkA());
   liftController.setP(liftconstants.getkP());
   liftController.setI(liftconstants.getkI());
@@ -49,6 +66,7 @@ public class ElevatorSubsystem extends SubsystemBase {
   }
 
   @Override
-  public void periodic() { 
+  public void periodic() {
+    m_elevatorMech2d.setLength(encoder.getPosition());
   }
 }
