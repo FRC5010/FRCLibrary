a7d1cdac4a01e7c3bd0cd19f0a8d32b668519300
Cole Razo <113945941+razocs1@users.noreply.github.com>
2023-02-14 17:54:18 -0500

    Create Intake Subsystem
    Set up the intake subsystem and command to toggle, still need command to spin the intake.

--- FRC5010Example2023/src/main/java/frc/robot/chargedup/ChargedUpMech.java
+++ FRC5010Example2023/src/main/java/frc/robot/chargedup/ChargedUpMech.java
@@ -5,10 +5,7 @@
 package frc.robot.chargedup;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
@@ -21,10 +18,12 @@ import frc.robot.FRC5010.motors.hardware.MotorModelConstants;
 import frc.robot.FRC5010.sensors.Controller;
 import frc.robot.commands.ElevatorMove;
 import frc.robot.commands.ElevatorOut;
+import frc.robot.commands.IntakeToggle;
 
 /** Add your docs here. */
 public class ChargedUpMech extends GenericMechanism {
     private ElevatorSubsystem elevatorSubsystem;
+    private IntakeSubsystem intakeSubsystem;
 
     public ChargedUpMech(Mechanism2d robotMechVisual, ShuffleboardTab shuffleTab) {
         super(robotMechVisual, shuffleTab);
@@ -34,6 +33,7 @@ public class ChargedUpMech extends GenericMechanism {
                 new MotorModelConstants(1, 1, 1), 
                 new MotorModelConstants(1, 1, 1),
                 mechVisual);
+        // TODO: Set up IntakeSubsystem once built
     }
 
     @Override
@@ -46,6 +46,13 @@ public class ChargedUpMech extends GenericMechanism {
                 .whileTrue(new ElevatorOut(elevatorSubsystem, () -> -0.1));
         driver.createXButton()
                 .whileTrue(new ElevatorOut(elevatorSubsystem, () -> 0.1));
+        //TODO: Add finish intake commands
+        //operator.createBButton()
+                //.onTrue(new IntakeToggle(intakeSubsystem));
+        //operator.createLeftTrigger();
+                //.onTrue(new IntakeSpin(intakeSubsystem, () -> -0.1));
+        //operator.createRightTrigger();
+                //.onTrue(new IntakeSpin(intakeSubsystem, () -> 0.1));
         operator.setRightYAxis(driver.createRightYAxis().deadzone(.07).negate());
         operator.setLeftYAxis(driver.createLeftYAxis().deadzone(0.07));
     }
