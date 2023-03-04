18a23842c8354866e30a7224ccbef2b79c590330
Cattown7 <jedibox630@gmail.com>
2023-03-04 16:12:16 -0500

    Auto Path Update

--- FRC5010Example2023/src/main/java/frc/robot/FRC5010/robots/PracticeBot.java
+++ FRC5010Example2023/src/main/java/frc/robot/FRC5010/robots/PracticeBot.java
@@ -98,7 +98,7 @@ public class PracticeBot extends GenericMechanism {
     autoMaps.addPath("8-1 North Cone", new PathConstraints(4, 3));
     autoMaps.addPath("7-2 North Cone", new PathConstraints(1, 0.5));
     autoMaps.addPath("6-3 South Cone", new PathConstraints(4, 3));
-    autoMaps.addPath("Bal 8-1 North Cone", new PathConstraints(4, 3));
+    autoMaps.addPath("Bal 7-2 Cube", new PathConstraints(1.5, .75));
   }
 
   @Override
