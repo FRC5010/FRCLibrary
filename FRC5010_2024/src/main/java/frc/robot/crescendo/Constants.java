d6d349916678f466aaf6856e0b647ecd23c227d1
Adam-Kolt <adamkoltuniuk@gmail.com>
2024-02-10 00:16:48 -0500

    Refactored AutoAim

--- FRC5010_2024/src/main/java/frc/robot/crescendo/Constants.java
+++ FRC5010_2024/src/main/java/frc/robot/crescendo/Constants.java
@@ -1,23 +1,24 @@
 package frc.robot.crescendo;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 public final class Constants {
     public static final class Physical {
         public static final double TRACK_WIDTH_INCHES = 16.8;
         public static final double WHEEL_BASE_INCHES = 21.5;
-        
+        public static final Transform3d PIVOT_ORIGIN_OFFSET = new Transform3d(new Translation3d(0.0, 0.0, 0.0),
+                new Rotation3d());
+
     }
+
     public static final class Field {
-        public static final double SPEAKER_X = 652.73;
-        public static final double SPEAKER_Y = 218.42;
-        public static final double SPEAKER_Z = 0.0;
-
-        public static final double SHOT_OFFSET_X = 0.0;
-        public static final double SHOT_OFFSET_Y = -9.055;
-        public static final double SHOT_OFFSET_Z = 80.515;
-
-        public static final double SHOT_X = SPEAKER_X + SHOT_OFFSET_X;
-        public static final double SHOT_Y = SPEAKER_X + SHOT_OFFSET_Y;
-        public static final double SHOT_Z = SPEAKER_X + SHOT_OFFSET_Z;
-
+        public static final Transform3d SPEAKER_POSE = new Transform3d(new Translation3d(652.73, 218.42, 0.0),
+                new Rotation3d());
+        private static final Transform3d SHOT_OFFSET = new Transform3d(new Translation3d(0.0, -9.055, 80.515),
+                new Rotation3d());
+        public static final Transform3d SHOT_POSE = SPEAKER_POSE.plus(SHOT_OFFSET);
     }
 }
--- FRC5010_2024/src/main/java/frc/robot/crescendo/commands/AutoAim.java
+++ FRC5010_2024/src/main/java/frc/robot/crescendo/commands/AutoAim.java
@@ -16,24 +16,51 @@ import frc.robot.crescendo.Constants;
 import frc.robot.crescendo.ShooterSubsystem;
 
 public class AutoAim extends Command {
- 
+
   DrivetrainPoseEstimator robotPose;
   ShooterSubsystem shooterSubsystem;
   PivotSubsystem pivotSubsystem;
   Drive drive;
-  Pose3d targetPose = new Pose3d(new Translation3d(Constants.Field.SHOT_X, Constants.Field.SHOT_Y, Constants.Field.SHOT_Z), new Rotation3d());
+  Transform3d targetPose;
+
   /** Creates a new AutoAim. */
   public AutoAim(PivotSubsystem pivotSubsystem, ShooterSubsystem shooterSubsystem, Drive drive) {
-    
+
+    targetPose = Constants.Field.SHOT_POSE;
+
     this.robotPose = drive.getDrivetrain().getPoseEstimator();
     this.pivotSubsystem = pivotSubsystem;
     this.shooterSubsystem = shooterSubsystem;
     this.drive = drive;
 
-    double shooterSpeed = 6000;
-    
     addRequirements(pivotSubsystem, shooterSubsystem);
-    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  public AutoAim(PivotSubsystem pivotSubsystem, ShooterSubsystem shooterSubsystem, Drive drive,
+      Transform3d targetPose) {
+    this.targetPose = targetPose;
+
+    this.robotPose = drive.getDrivetrain().getPoseEstimator();
+    this.pivotSubsystem = pivotSubsystem;
+    this.shooterSubsystem = shooterSubsystem;
+    this.drive = drive;
+
+    addRequirements(pivotSubsystem, shooterSubsystem);
+  }
+
+  private double computeYawBetweenTransforms(Transform3d from, Transform3d to) {
+    Translation3d difference = from.getTranslation().minus(to.getTranslation());
+    double yawAngle = Math.atan2(difference.getY(), difference.getX());
+    yawAngle += Math.PI / 2;
+    yawAngle = Math.toDegrees(yawAngle);
+    return yawAngle;
+  }
+
+  private double computePitchBetweenTransforms(Transform3d from, Transform3d to) {
+    Translation3d difference = from.getTranslation().minus(to.getTranslation());
+    double hypotenousA = Math.sqrt(Math.pow(difference.getX(), 2) + Math.pow(difference.getY(), 2));
+    double pivotAngle = Math.atan2(difference.getZ(), hypotenousA);
+    return Math.toDegrees(pivotAngle);
   }
 
   // Called when the command is initially scheduled.
@@ -45,21 +72,24 @@ public class AutoAim extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Transform3d difference = robotPose.getCurrentPose3d().minus(targetPose);
-    double hypotenousA = Math.sqrt(Math.pow(difference.getX(), 2) + Math.pow(difference.getY(), 2));
-    double pivotAngle = Math.atan2(difference.getZ(), hypotenousA);
-    double robotRawAngle = Math.atan2(difference.getY(), difference.getX());
-    double robotAngle;
-    if (robotRawAngle < 0) {
-      robotAngle = -90 - robotRawAngle;
-    } else {
-      robotAngle = 90 - robotRawAngle;
-    }
+    Translation3d robotTranslation = robotPose.getCurrentPose3d().getTranslation();
+    // TODO: Have to account for robot rotation just like with camera
+    Transform3d pivotOrigin = new Transform3d(
+        robotTranslation.plus(Constants.Physical.PIVOT_ORIGIN_OFFSET.getTranslation()), new Rotation3d());
+
+    double yaw = computeYawBetweenTransforms(pivotOrigin, targetPose);
+    double pitch = computePitchBetweenTransforms(pivotOrigin, targetPose);
+
+    pivotSubsystem.setReference(pitch);
+
+    // TODO: Rotate Drive to Target and Allow for Joystick Translation
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
